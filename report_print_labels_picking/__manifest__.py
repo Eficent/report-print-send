1f4063f0066e770a5b606bc534df1d8dc9af7b6d
Núria Martín Xifré <nuria.martin@eficent.com>
2019-11-12 12:12:33 +0100

    New module Print labels in a picking

---
+++ report_print_labels_picking/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Eficent <patrickraymondwilson@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Print Labels in Picking',
+    'summary': "Print Product Labels in a STock Picking",
+    'author': "Eficent, Odoo Community Association (OCA)",
+    'website': "https://github.com/eficent",
+    'category': 'Customer Relationship Management',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock'
+        ],
+    'data': [
+        'reports/report_print_labels.xml',
+        'reports/report_print_labels_template.xml'
+
+    ],
+    'development_status': 'Beta',
+    'maintainers': ['NuriaMartinXifre'],
+}
